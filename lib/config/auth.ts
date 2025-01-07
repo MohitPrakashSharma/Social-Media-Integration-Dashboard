2729c8e2d23648f5750e0253856e05e629345d80
Mohit Sharma <mohi@mediahubster.com>
2025-01-07 18:09:39 +0530

    new update

--- lib/config/auth.ts
+++ lib/config/auth.ts
@@ -1,11 +1,12 @@
 export const authConfig = {
 	twitter: {
-		clientId: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID,
-		clientSecret: process.env.TWITTER_CLIENT_SECRET,
+		clientId: process.env.NEXT_PUBLIC_TWITTER_CONSUMER_KEY || 'HaTwg5xZ3zxeFPdZJj1IaxX8s',
+		clientSecret: process.env.TWITTER_CONSUMER_SECRET || 'hd5hFlypdBXOKw0wTx2fEQDDKLezIz8qYSabktlB6Ei6M4OOwN',
 		redirectUri: 'https://social.tdx.biz/api/auth/twitter/callback',
-		authUrl: 'https://twitter.com/i/oauth2/authorize',
-		tokenUrl: 'https://api.twitter.com/2/oauth2/token',
-		scope: ['tweet.read', 'users.read'],
+		authUrl: 'https://api.twitter.com/oauth/authorize',
+		requestTokenUrl: 'https://api.twitter.com/oauth/request_token',
+		accessTokenUrl: 'https://api.twitter.com/oauth/access_token',
+		scope: ['tweet.read', 'users.read', 'offline.access'],
 	},
 	youtube: {
 		clientId: process.env.NEXT_PUBLIC_YOUTUBE_CLIENT_ID,
